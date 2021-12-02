14fd615086043f4683fcdb4f4a389515f2c0f557
michaelblanco <guesswhoblanco@gmail.com>
2021-12-02 15:14:29 -0500

    LAB 13 COMPLETED, THANKS FOR EVERYTHING WE STRUGGLE WHEN YOU WERENT HERE:D

--- labs/lab13/lab13.py
+++ labs/lab13/lab13.py
@@ -2,10 +2,64 @@
 Name: <your name goes here – first and last>
 <ProgramName>.py
 """
+from random import randint
+def ibb(value, list1):
+    left_p = 0
+    right = len(list1) - 1
+    while left_p <= right:
+        middle = (left_p + right) // 2
+        middle_value = list1[middle]
+        if middle_value == value:
+            print(middle)
+            break
+        elif value < middle_value:
+            right = middle_value
 
-def main():
-    # add other function calls here
-    pass
+        elif value > middle_value:
+            left_p = middle + 1
+#
+#
+def ss(list1):
+    for i in range(len(list1)):
+        lowest = list1[i]
+        pos = i
+        for index in range(i, len(list1)):
+            if list1[index] < list1[lowest]:
+                lowest = list1[index]
+                pos = index
+        list1[i], list1[pos] = list1[pos], list1[i]
+
+#
+#
+def get_area(rec):
+    return randint(1, 100)
 
 
-main()
+def rect_sort(values):
+    d = {}
+    areas = []
+    for rectangles in values:
+        d[get_area(rectangles)] = rectangles
+        areas.append(get_area(rectangles))
+    ss(areas)
+    for i in range(len(areas)):
+        values[i] = d[areas]
+    return areas
+
+
+def trade_alert(filename):
+    file = open(filename, "r")
+    data = file.read().split(" ")
+    for i in range(len(data)):
+        if 500 < int(data[i]) < 830:
+            print(i + 1, "warning")
+        elif int(data[i]) > 830:
+            print(i + 1, "ALERT!!!")
+
+
+
+if __name__ == '__main__':
+    ibb(9, [1,2,3,4,5,6,7,8,9])
+
+
+
